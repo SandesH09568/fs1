ddaa8a0cc1fbe3db48d49ace66bb3c37d095d3a7
SandesH09568 <sandeshmore095@gmail.com>
2021-08-20 19:11:01 +0530

    session 10complted

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/models/catalog.dart';
 import 'package:flutter_application_1/widgets/drawer.dart';
+import 'package:flutter_application_1/widgets/item_widget.dart';
 
 class Homepage extends StatelessWidget {
   final int days = 30;
@@ -7,14 +9,25 @@ class Homepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(5, (index) => CatalogModel.items[0]);
     return Scaffold(
-      appBar: AppBar(title: Text("Catlog App")),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to Home $days $name"),
+      appBar: AppBar(
+        title: Text(
+          "Catlog App",
+          // textAlign: TextAlign.center,
         ),
+      ),
+      body: ListView.builder(
+        itemCount: dummyList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ItemWidget(
+            item: dummyList[index],
+          );
+        },
       ),
       drawer: Mydrawer(),
     );
   }
 }
+// elevation: 0.0,
+        // backgroundColor: Colors.white,// style: TextStyle(color: Colors.black),// iconTheme: IconThemeData(color: Colors.black),
