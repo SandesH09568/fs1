7656cfbba53a57aa9b56f540d6d2ebdd87513940
SandesH09568 <sandeshmore095@gmail.com>
2021-08-20 20:40:57 +0530

    session11 part1

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_application_1/models/catalog.dart';
 import 'package:flutter_application_1/widgets/drawer.dart';
 import 'package:flutter_application_1/widgets/item_widget.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
   final int days = 30;
+
   final String name = "Coding";
 
   @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    // print(catalogJson);
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    print(productsData);
+  }
+
   Widget build(BuildContext context) {
     final dummyList = List.generate(5, (index) => CatalogModel.items[0]);
     return Scaffold(
